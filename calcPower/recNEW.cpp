10dfd8bf04b909ff7ff17d1e1f722dd99d21f78a
sanatsawant <sanatsawantgvms@gmail.com>
2026-02-04 16:09:22 +0530

    Update recNEW.cpp

--- calcPower/recNEW.cpp
+++ calcPower/recNEW.cpp
@@ -1,11 +1,11 @@
-// const int M=1e9+7;
+const int M=1e9+7;
 
-// int powm(int a, int b) {
-//     if (b == 0) return 1;
-//     ll res = powm(a, b / 2); // Use long long for intermediate results
-//     res = (res * res) % M;  // Squaring the result
-//     if (b & 1) {            // If b is odd
-//         res = (res * a) % M;
-//     }
-//     return res;
-// }
+int powm(int a, int b) {
+    if (b == 0) return 1;
+    ll res = powm(a, b / 2); 
+    res = (res * res) % M; 
+    if (b & 1) {         
+        res = (res * a) % M;
+    }
+    return res;
+}
